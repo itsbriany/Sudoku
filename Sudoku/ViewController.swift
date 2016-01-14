6fccff268b21472829f181d3518cb41766a989ae
Brian Yip <itsbriany@gmail.com>
2016-01-14 15:53:53 -0400

    Adding files

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Brian Yip on 2016-01-14.
+//  Copyright © 2016 Brian Yip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
