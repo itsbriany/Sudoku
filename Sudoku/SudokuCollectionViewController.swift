4626e08a5722f9f0e4382dae914f77a60df75ceb
Brian Yip <itsbriany@gmail.com>
2016-01-20 12:35:57 -0400

    Working on making the grid fit for all screen resolutions.

--- Sudoku/SudokuCollectionViewController.swift
+++ Sudoku/SudokuCollectionViewController.swift
@@ -11,27 +11,46 @@ import UIKit
 class SudokuCollectionViewController: UICollectionViewController {
    
     // MARK: Properties
+    let cellRows = 9
+    let cellColumns = 9
     let sudokuCollectionViewControllerCellIdentifier = "BoxCollectionViewCell"
-    let sudokuCollectionViewCells = [BoxCollectionViewCell!](count: 9, repeatedValue: nil)
     let sudokuCollectionViewCellBorderWidth: CGFloat = 1
     let sudokuCollectionViewCellBorderColor = UIColor.blackColor().CGColor
     
     // MARK: UICollectionViewController overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView!.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     override func collectionView(collectionView: UICollectionView,
         cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(sudokuCollectionViewControllerCellIdentifier,
-                forIndexPath: indexPath)
-            cell.layer.borderWidth = sudokuCollectionViewCellBorderWidth
-            cell.layer.borderColor = sudokuCollectionViewCellBorderColor
-            return cell
+        return formatCell(indexPath)
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sudokuCollectionViewCells.count
+        return getSudokuCollectionViewCells().count
     }
+    
+    // MARK: Helpers
+    func formatCell(indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView!.dequeueReusableCellWithReuseIdentifier(sudokuCollectionViewControllerCellIdentifier,
+            forIndexPath: indexPath) as! BoxCollectionViewCell
+        cell.layer.borderWidth = sudokuCollectionViewCellBorderWidth
+        cell.layer.borderColor = sudokuCollectionViewCellBorderColor
+        // TODO The cells are overlapping!
+        cell.frame = customCellFrame(0, y: 0)
+        return cell
+    }
+    
+    func customCellFrame(x: CGFloat, y: CGFloat) -> CGRect {
+        let screenSize = UIScreen.mainScreen().bounds
+        let cellSize = screenSize.width / CGFloat(cellColumns)
+        return CGRect(x: x, y: y, width: cellSize, height: cellSize)
+    }
+    
+    func getSudokuCollectionViewCells() -> [BoxCollectionViewCell!] {
+        let sudokuCollectionViewCells = [BoxCollectionViewCell!](count: (cellRows * cellColumns), repeatedValue: nil)
+        return sudokuCollectionViewCells
+    }
+
 }
